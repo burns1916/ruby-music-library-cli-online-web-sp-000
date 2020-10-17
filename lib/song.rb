e3c78197377a971f4a15b2840c5e723fb87466d8
Ryan K Burns <ryankburns@gmail.com>
2020-10-17 23:39:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,12 +34,9 @@ class Song
     new_song
   end
 
-  def artist_name=(name)
-    artist.add_song(self)
-  end
-
   def artist=(artist)
     @artist = artist
+    artist.add_song(self)
   end
 
   def genre=(genre)

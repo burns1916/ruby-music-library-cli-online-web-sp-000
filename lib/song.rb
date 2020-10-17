cad9ad8cada4d995c2545159bfa8eb3202b567b5
Ryan K Burns <ryankburns@gmail.com>
2020-10-17 22:58:55 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,64 @@
+class Song
+
+  attr_accessor :name
+  attr_reader :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    if artist
+      self.artist = artist
+    if genre
+      self.genre = genre
+    end
+    end
+    @@all << self
+    end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(song)
+    new_song = self.new(song)
+    new_song
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+    end
+
+  def self.find_by_name(song_name)
+    self.all.find{|songs| songs.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if song = self.find_by_name(song_name)
+    song
+    else
+      self.create(song_name)
+    end
+  end
+
+  def self.new_from_filename(filename)
+    song = filename.split(" - ")[1]
+    artist = filename.split(" - ")[0]
+    genre = filename.split( " - ")[2]
+    new_song = self.new(song, artist, genre)
+  end
+end

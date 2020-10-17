6c4e54505759edacfdc641fd1b4337cce56273f9
Ryan K Burns <ryankburns@gmail.com>
2020-10-17 23:31:51 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -10,8 +10,4 @@ class MusicImporter
     @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
   end
 
-  def import
-    files.each{|filename| Song.new_from_filename(filename)}
-  end
-
 end

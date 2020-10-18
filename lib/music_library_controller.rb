60d566578d7ea7ccee8bfd53f8e2ad77fe23e5d9
Ryan K Burns <ryankburns@gmail.com>
2020-10-18 01:06:53 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,22 +1,27 @@
 class MusicLibraryController
-  def initialize(path = './db/mp3s')
+  attr_accessor :path
+
+  def initialize(path='./db/mp3s')
+    @path = path
     MusicImporter.new(path).import
   end
 
   def call
-    input = nil
-    while (input != 'exit')
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    input = gets.chomp
-    case input
+     input = gets.strip
+
+     if input != exit
+
+     puts "Welcome to your music library!"
+     puts "To list all of your songs, enter 'list songs'."
+     puts "To list all of the artists in your library, enter 'list artists'."
+     puts "To list all of the genres in your library, enter 'list genres'."
+     puts "To list all of the songs by a particular artist, enter 'list artist'."
+     puts "To list all of the songs of a particular genre, enter 'list genre'."
+     puts "To play a song, enter 'play song'."
+     puts "To quit, type 'exit'."
+     puts "What would you like to do?"
+
+     case input
          when "list songs"
            list_songs
          when "list artists"
@@ -30,13 +35,13 @@ class MusicLibraryController
          when "play song"
            play_song
          end
-    end
-    end
-  end
-  private
+     end
+   end
+
   def list_songs
-     Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
-       puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
+      puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    end
   end
 
   def list_artists
@@ -82,6 +87,5 @@ class MusicLibraryController
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
-
 
 end

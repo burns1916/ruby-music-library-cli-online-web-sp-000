d21dc5af445d8c9d41e360bde47cc81e16923d69
Ryan K Burns <ryankburns@gmail.com>
2020-10-18 00:59:30 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -40,8 +40,6 @@ class MusicLibraryController
     end
   end
 
-  private
-
   def list_songs
     Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
       puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
